910fde6abc4a40c7baefd86ce336133000292148
mibbp <2140078954@qq.com>
2023-06-05 16:53:26 +0800

    实现蛇

--- web/src/assets/scripts/Snake.js
+++ web/src/assets/scripts/Snake.js
@@ -22,6 +22,23 @@ export class Snake extends AcGameObject {
 
         this.step = 0;
         this.eps = 1e-2;
+
+        this.eye_directions = 0;
+        if (this.id === 1) this.eye_directions = 2;
+
+        this.eye_dx = [
+            [-1, 1],
+            [1, 1],
+            [-1, 1],
+            [-1, -1]
+        ];
+        this.eye_dy = [
+            [-1, -1],
+            [-1, 1],
+            [1, 1],
+            [-1, 1]
+        ];
+
     }
 
 
@@ -45,11 +62,14 @@ export class Snake extends AcGameObject {
         this.direction = -1;
         this.status = "move";
         this.step++;
+        this.eye_directions = d;
 
         const k = this.cells.length;
         for (let i = k; i > 0; i--) {
             this.cells[i] = JSON.parse(JSON.stringify(this.cells[i - 1]));
         }
+        if (!this.gamemap.check_valid(this.next_cell))
+            this.status = "die";
     }
 
     update_move() {
@@ -90,7 +110,9 @@ export class Snake extends AcGameObject {
         const L = this.gamemap.L;
         const ctx = this.gamemap.ctx;
         ctx.fillStyle = this.color;
-
+        if (this.status === "die") {
+            ctx.fillStyle = "white";
+        }
         for (const cell of this.cells) {
             ctx.beginPath();
             ctx.arc(cell.x * L, cell.y * L, L / 2 * 0.8, 0, 2 * Math.PI);
@@ -107,6 +129,14 @@ export class Snake extends AcGameObject {
             else {
                 ctx.fillRect(Math.min(a.x, b.x) * L, (a.y - 0.4) * L, Math.abs(a.x - b.x) * L, L * 0.8);
             }
+        }
+        ctx.fillStyle = "black";
+        for (let i = 0; i < 2; i++) {
+            const eye_x = (this.cells[0].x + this.eye_dx[this.eye_directions][i] * 0.15) * L;
+            const eye_y = (this.cells[0].y + this.eye_dy[this.eye_directions][i] * 0.15) * L;
+            ctx.beginPath();
+            ctx.arc(eye_x, eye_y, L * 0.05, 0, Math.PI * 2);
+            ctx.fill();
         }
     }
 
